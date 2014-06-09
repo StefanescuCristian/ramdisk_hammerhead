7ea874ad0316a8be2dfd8369c1d61817bf4aaa32
Stefanescu Cristian <howerkraft@gmail.com>
2014-06-09 13:36:20 +0300

    give KitKat launcher some love

--- boot.img-ramdisk/sbin/fkbootscript.sh
+++ boot.img-ramdisk/sbin/fkbootscript.sh
@@ -35,7 +35,7 @@ while sleep 1; do
 done&
 
 # lmk whitelist for common launchers and increase launcher priority
-list="com.android.launcher com.google.android.googlequicksearchbox org.adw.launcher org.adwfreak.launcher net.alamoapps.launcher com.anddoes.launcher com.android.lmt com.chrislacy.actionlauncher.pro com.cyanogenmod.trebuchet com.gau.go.launcherex com.gtp.nextlauncher com.miui.mihome2 com.mobint.hololauncher com.mobint.hololauncher.hd com.qihoo360.launcher com.teslacoilsw.launcher com.tsf.shell org.zeam";
+list="com.android.launcher com.google.android.googlequicksearchbox org.adw.launcher org.adwfreak.launcher net.alamoapps.launcher com.anddoes.launcher com.android.lmt com.chrislacy.actionlauncher.pro com.cyanogenmod.trebuchet com.gau.go.launcherex com.gtp.nextlauncher com.miui.mihome2 com.mobint.hololauncher com.mobint.hololauncher.hd com.qihoo360.launcher com.teslacoilsw.launcher com.tsf.shell org.zeam com.kk.launcher";
 while sleep 60; do
   for class in $list; do
     if [ `$bb pgrep $class | head -n 1` ]; then
