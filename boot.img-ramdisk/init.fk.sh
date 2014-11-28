991a507960a7c299929acc7e95c74e19f2994bdd
Stefanescu Cristian <howerkraft@gmail.com>
2014-11-28 23:32:01 +0200

    disable touchboost
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- boot.img-ramdisk/init.fk.sh
+++ boot.img-ramdisk/init.fk.sh
@@ -31,3 +31,4 @@ echo "simple" > /sys/class/kgsl/kgsl-3d0/pwrscale/trustzone/governor
 #others
 echo 2 > /sys/devices/system/cpu/sched_mc_power_savings
 echo 1 > /sys/module/snd_soc_wcd9320/parameters/high_perf_mode
+touch /dev/socket/pb && chmod 444 /dev/socket/pb
